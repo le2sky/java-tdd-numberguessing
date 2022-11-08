25cb89e7494fcaa591282d68a8af0cb03b64cb69
leesky <leehaneul0623@gmail.com>
2022-11-08 19:08:33 +0900

    feat(AppModel) : processInput 개선
    - RED : 입력되는 숫자가 answer보다 클 경우 예상되는 메시지 출력
    - GREEN : 입력된 숫자에 따라서 클 경우 표시되는 메시지 출력하도록 개선
    - REFACTOR : pass

--- console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -10,8 +10,7 @@ public final class AppModel {
 
     public AppModel(PositiveIntegerGenerator generator) {
         completed = false;
-        output = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit"
-                + NEW_LINE + "Enter selection: ";
+        output = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit" + NEW_LINE + "Enter selection: ";
         answer = generator.generateLessThanOrEqualToHundread();
     }
 
@@ -25,18 +24,18 @@ public final class AppModel {
 
     public void processInput(String input) {
         if (input.equals("1")) {
-            output = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
-                    + NEW_LINE + "Enter your guess: ";
+            output = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100." + NEW_LINE + "Enter your guess: ";
             return;
-        }
-        else if (input.equals("3")) {
+        } else if (input.equals("3")) {
             completed = true;
             return;
-        } ;
+        }
 
         int guess = Integer.parseInt(input);
         if (guess < answer) {
             output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
+        } else if(guess > answer){
+            output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
         }
 
     }
