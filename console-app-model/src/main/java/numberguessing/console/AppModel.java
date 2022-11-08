160aba37a7762996ce0c79e60957f424cba2e664
leesky <leehaneul0623@gmail.com>
2022-11-08 23:10:09 +0900

    feat(AppModel) : processSinglePlayerGame 개선
    - RED : 게임이 종료되면 모드 선택 메시지 출력 예상
    - GREEN : 게임이 종료될 경우 모드 선택 메시지 출력하도록 개선
    - REFACTOR : 모드 선택 메시지 상수화

--- console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -4,6 +4,7 @@ import numberguessing.PositiveIntegerGenerator;
 
 public final class AppModel {
     private final static String NEW_LINE = System.lineSeparator();
+    public static final String SELECT_MODE_MESSAGE = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit" + NEW_LINE + "Enter selection: ";
     private boolean completed;
     private String output;
     private int answer;
@@ -12,7 +13,7 @@ public final class AppModel {
 
     public AppModel(PositiveIntegerGenerator generator) {
         completed = false;
-        output = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit" + NEW_LINE + "Enter selection: ";
+        output = SELECT_MODE_MESSAGE;
         answer = generator.generateLessThanOrEqualToHundread();
         singlePlayerMode = false;
         tries = 0;
@@ -42,7 +43,7 @@ public final class AppModel {
         } else if (guess > answer) {
             output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
         } else {
-            output = "Correct! " + tries + (tries == 1 ? " guess." : " guesses.") + NEW_LINE;
+            output = "Correct! " + tries + (tries == 1 ? " guess." : " guesses.") + NEW_LINE + SELECT_MODE_MESSAGE;
         }
     }
 
--- console-app-model/src/test/java/numberguessing/console/AppModel_specs.java
+++ console-app-model/src/test/java/numberguessing/console/AppModel_specs.java
@@ -122,19 +122,19 @@ public class AppModel_specs {
 
         assertThat(actual).contains("1 guess.");
     }
-//
-//    @Test
-//    void sut_prints_select_mode_message_if_single_player_game_finished() {
-//        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
-//        sut.processInput("1");
-//        sut.flushOutput();
-//        sut.processInput("50");
-//
-//        String actual = sut.flushOutput();
-//
-//        assertThat(actual).endsWith("1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit"
-//                + NEW_LINE + "Enter selection: ");
-//    }
+
+    @Test
+    void sut_prints_select_mode_message_if_single_player_game_finished() {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("1");
+        sut.flushOutput();
+        sut.processInput("50");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).endsWith("1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit"
+                + NEW_LINE + "Enter selection: ");
+    }
 //
 //    @Test
 //    void sut_returns_to_mode_selection_if_single_player_game_finished() {
